b6aa3764f2c41662dd34287f846757329048d0f9
huybang2017 <ndhbang2017@gmail.com>
2023-05-17 15:40:20 +0700

    fixign

--- src/modules/Booking/redux/getDataTicketSlice.js
+++ src/modules/Booking/redux/getDataTicketSlice.js
@@ -1,13 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { getDataTicket } from "../../../apis/bookingAPI";
-const initialState = { dataTicket: null };
+const initialState = { dataTicket: null, isLoading: false, error: null };
 const bookingSlice = createSlice({
   name: "booking",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
+    builder.addCase(getDataTicket.pending, (state) => {
+      return { ...state, isLoading: true, error: null };
+    });
     builder.addCase(getDataTicket.fulfilled, (state, action) => {
-      return { ...state, dataTicket: action.payload };
+      return { ...state, isLoading: false, dataTicket: action.payload };
+    });
+    builder.addCase(getDataTicket.rejected, (state, action) => {
+      return { ...state, isLoading: false, error: action.error.message };
     });
   },
 });
